8342b9705c0255c88e4ada634be4b2879acc220c
abdulkader33447 <abdulkader33447@gmail.com>
2025-03-24 08:49:19 +0600

    added a card

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,56 @@
-import Counter from './Counter'
-import './App.css'
-import Batsman from './Batsman'
-import Bolar from './Bolar'
-
+import Counter from "./Counter";
+import "./App.css";
+import Batsman from "./Batsman";
+import Bolar from "./Bolar";
+import Users from "./Users";
 
 function App() {
-
-  function handleClick(){
-    alert('i am clicked')
+  function handleClick() {
+    alert("i am clicked");
   }
 
-  const handleClick3=()=>{
-    alert('clicked 3')
-  }
+  const handleClick3 = () => {
+    alert("clicked 3");
+  };
 
-  const handleClickAdd5= (num)=>{
-    const newNum = num +5;
-    alert(newNum)
-  }
+  const handleClickAdd5 = (num) => {
+    const newNum = num + 5;
+    alert(newNum);
+  };
 
   return (
     <>
-      
       <h1>Vite + React</h1>
-<Bolar></Bolar>
+      <Users></Users>
 
-<Batsman></Batsman>
-<Counter></Counter>
+      <Bolar></Bolar>
+
+      <Batsman></Batsman>
+      <Counter></Counter>
 
       <button onClick={handleClick}>Click Me</button>
 
-      <button onClick={function handleClick2(){alert('clicked 2')}}>Click Me 2</button>
+      <button
+        onClick={function handleClick2() {
+          alert("clicked 2");
+        }}
+      >
+        Click Me 2
+      </button>
 
       <button onClick={handleClick3}>Click Me 3</button>
-      
-      <button onClick={()=>{alert('clicked 4')}}>Click Me 4</button>
 
-      <button onClick={()=>handleClickAdd5(5)}>Click add 5</button>
+      <button
+        onClick={() => {
+          alert("clicked 4");
+        }}
+      >
+        Click Me 4
+      </button>
+
+      <button onClick={() => handleClickAdd5(5)}>Click add 5</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
